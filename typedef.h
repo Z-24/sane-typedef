19ed7e2f117ef130c13be672c696d25fbf77f50a
Z-24 <62087934+Z-24@users.noreply.github.com>
2021-12-24 17:13:43 -0500

    the only commit
    This repository only consists of one file. Self explanatory.

---
+++ typedef.h
@@ -0,0 +1,7 @@
+//header guard format is CREATOR + FILENAME (without extension) + DATE OF CREATION (h/mm dd/mm yyyy) + _H
+#ifndef Z24_TYPEDEF_63525112021_H
+#define Z24_TYPEDEF_63525112021_H
+
+#define typedef(x, y) typedef y x
+
+#endif

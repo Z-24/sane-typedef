9c159dafc4a0b72d4a7e99203352071145c76542
Z-24 <62087934+Z-24@users.noreply.github.com>
2021-12-24 17:23:36 -0500

    Correct description of header guard format

--- typedef.h
+++ typedef.h
@@ -1,4 +1,4 @@
-//header guard format is CREATOR + FILENAME (without extension) + DATE OF CREATION (h/mm dd/mm yyyy) + _H
+//header guard format is CREATOR + FILENAME (without extension) + DATE OF CREATION (hh/mm dd/mm yyyy) + _H
 #ifndef Z24_TYPEDEF_63525112021_H
 #define Z24_TYPEDEF_63525112021_H
 
